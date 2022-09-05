316bbc0a760521c58adcfc9de34ede63f4f84e91
Kate Murray <ktaymur@ad.unc.edu>
2022-09-04 20:19:23 -0400

    final product

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,12 +2,12 @@
 __author__ = "730556906"
 
 five_character_word: str = input("Enter a 5-character word: ")
-if len(five_character_word)!= 5:
+if len(five_character_word) != 5:
     print("Error: Word must contain 5 characters ")
     exit()
 
 character_guess: str = input("Enter a single character: ")
-if len(character_guess)!=1:
+if len(character_guess) != 1:
     print("Error: Character must be a single character. ")
     exit()
 
@@ -15,19 +15,19 @@ print("Searching for " + character_guess + " in " + five_character_word)
 
 letter_instances: int = 0
 
-if five_character_word[0]==character_guess:
+if five_character_word[0] == character_guess:
     print(character_guess + " found at index 0 ")
     letter_instances = letter_instances + 1
-if five_character_word[1]==character_guess:
+if five_character_word[1] == character_guess:
     print(character_guess + " found at index 1 ")
     letter_instances = letter_instances + 1
-if five_character_word[2]==character_guess:
+if five_character_word[2] == character_guess:
     print(character_guess + " found at index 2 ")
     letter_instances = letter_instances + 1
-if five_character_word[3]==character_guess:
+if five_character_word[3] == character_guess:
     print(character_guess + " found at index 3 ")
     letter_instances = letter_instances + 1
-if five_character_word[4]==character_guess:
+if five_character_word[4] == character_guess:
     print(character_guess + " found at index 4 ")
     letter_instances = letter_instances + 1
 
---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,37 @@
+"""One shot wordle"""
+from platform import python_branch
+
+
+__author__: str = 730556906
+
+secret: str = "python"
+guess: str = input(f"What is your {len(secret)}-letter guess? ")
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+while len(guess) != len(secret):
+    guess = input(f"That was not {len(secret)} letters! Try again: ")
+
+i: int = 0
+s: str = ""
+while i < len(secret):
+    if secret[i] == guess[i]:
+        s = s + GREEN_BOX
+    else:
+        index_match: int = 0
+        character_present: bool = False
+        while character_present == False and index_match < len(secret):
+            if secret[index_match] == guess[i]:
+                s = s + YELLOW_BOX
+                character_present = True
+            else:
+                index_match += 1
+        if character_present == False:
+            s = s + WHITE_BOX
+        
+    i = i + 1
+print(s)
+if len(guess) == len(secret) and guess == secret:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
